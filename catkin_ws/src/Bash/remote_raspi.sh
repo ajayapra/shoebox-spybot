0f3bf9d363440f2a8e4e4dffa248742edf4a68aa
ajayapra <ajayapra@mtu.edu>
2017-06-12 14:40:15 -0400

    Updated remote connection bash script

--- catkin_ws/src/Bash/remote_raspi.sh
+++ catkin_ws/src/Bash/remote_raspi.sh
@@ -1,3 +1,3 @@
 export ROS_MASTER_URI=http://pi-desktop:11311   # Hostname for Jackal 2
-export ROS_IP="$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+export ROS_IP=`hostname -I`
 
